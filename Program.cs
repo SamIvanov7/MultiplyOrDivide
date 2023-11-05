654baac5e2d968225833e8dd33814152e22e87f5
Sam Ivanov <si@juno-solar.com>
2023-11-05 15:20:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiplyOrDivide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Result: " + number * 3);
+            }
+            else
+            {
+                Console.WriteLine("Result: " + (double)number / 2);
+            }
+        }
+    }
+}
